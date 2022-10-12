2e079eebb43e22fcd382bf3132ba695c314f7020
Roman <eeuriset8@gmail.com>
2022-10-12 23:08:22 +0300

    Writing a ThreadPool class, tests and comments

---
+++ ThreadPool/ThreadPool/MyThreadPoolTests/MyThreadPoolTests.cs
@@ -0,0 +1,148 @@
+namespace MyThreadPoolTests;
+
+public class Tests
+{ 
+    // ≈сли операци€ была отменена ожидаем икслючение
+    [Test]
+    public void ShouldOperationCanceledExceptionWhenShutDownIfTaskHasNotStart()
+    {
+        var pool = new MyThreadPool.MyThreadPool(1);
+        var task = pool.Submit(() => 1);
+        pool.ShutDown();
+        int ReturnResult() => task.Result;
+        Assert.Throws<OperationCanceledException>(() => ReturnResult());
+    }
+
+    [Test]
+    public void ShouldExpectedIsCancellationRequestedIsEqualTrueWhenShutDown()
+    {
+        var pool = new MyThreadPool.MyThreadPool(1);
+        pool.ShutDown();
+        Assert.That(pool.Source.Token.IsCancellationRequested, Is.EqualTo(true));
+    }
+
+    private static IEnumerable<TestCaseData> TestRemoveCaseData() => new TestCaseData[]
+    {
+    };
+
+    private static IEnumerable<TestCaseData> CaseData()
+    {
+        MyThreadPool.MyThreadPool pool = new (10);
+        var list = new List<MyThreadPool.IMyTask<int>>();
+        var numberOfTasks = 15;
+
+        for (int i = 0; i < numberOfTasks; i++)
+        {
+            var locali = i;
+            list.Add(pool.Submit(() => locali));
+        }
+
+        int counter = 0;
+        foreach (var value in list)
+        {
+            yield return new TestCaseData(value.Result, counter, pool);
+            counter++;
+        }
+    }
+
+    // ¬се задачи посчитались и мы провер€ем что результат верный
+    [TestCaseSource(nameof(CaseData))]
+    public void ShouldResultIsEqualExpectedValue(int actual, int expected, MyThreadPool.MyThreadPool pool)
+    {
+        Assert.That(actual, Is.EqualTo(expected));
+        pool.ShutDown();
+    }
+
+    // ѕровер€ем, что число потоков не уменьшаетс€ (в моем случае не мен€етс€ вообще)
+    [Test]
+    public void ShouldNumberOfThreadIsEqualNumberThatWasSentToConstructor()
+    {
+        int numberOfThreads = 10;
+        MyThreadPool.MyThreadPool pool = new(numberOfThreads);
+        var task = pool.Submit(() => 1);
+        task = pool.Submit(() => 1);
+        Assert.That(numberOfThreads, Is.EqualTo(pool.CountOfThreads));
+        pool.ShutDown();
+
+        numberOfThreads = 5;
+        pool = new(numberOfThreads);
+        Assert.That(numberOfThreads, Is.EqualTo(pool.CountOfThreads));
+        pool.ShutDown();
+
+        numberOfThreads = 100;
+        pool = new(numberOfThreads);
+        task = pool.Submit(() => 1);
+        int a = task.Result;
+        Assert.That(numberOfThreads, Is.EqualTo(pool.CountOfThreads));
+        pool.ShutDown();
+    }
+
+    // ѕроверим что нова€ задача будет исполнена не ранее, чем завершитс€ исходна€
+    // «наю что сон в 1000 мс это очень много дл€ теста, но не знаю как иначе проверить
+    // я его закомментирую, но если что он прошел
+    /*
+    [Test]
+    public void ShouldTheNewTaskWillBeCompletedNoEarlierThanTheOriginalOneIsCompleted()
+    {
+        var pool = new MyThreadPool.MyThreadPool(10);
+
+        // ƒелаем задачу с методом у которого врем€ исполнени€ > 1000 мс
+        var task = pool.Submit(() => { Thread.Sleep(1000); return 1; });
+
+        // ѕредположим, что нова€ задача не ждет исходную
+
+        // Ќова€ задача не очень трудоемка€ - поэтому, вроде как, 100 мс дл€ нее вполне достаточно (учитыва€ что пул из 10 потоков, из которых зан€т только 1)
+        static int ReturnTwo(int lol) => 2;
+        var continuation = task.ContinueWith(returnTwo);
+
+        // Ќова€ задача не ждет старую и должна исполнитьс€ за 100 мс (то есть до ShutDown)
+        // «начит при обращении к свойству Result все должно быть ок
+        Thread.Sleep(100);
+        pool.ShutDown();
+
+        bool isCompleted = false;
+
+        // ≈сли все плохо - то это противоречие. Ќо все хорошо => нова€ ждет исходную ( если мы верим в то, что нова€ задача успеет исполнитьс€ за 100 мс)
+        Assert.That(isCompleted, Is.EqualTo(continuation.IsCompleted));
+    }*/
+
+    // ѕроверим что новые задачи исполн€ютс€
+    [Test]
+    public void ShouldNewTaskIsBeingExecuted()
+    {
+        var pool = new MyThreadPool.MyThreadPool(10);
+        var task = pool.Submit(() => 1);
+
+        static int ReturnTwo(int lol) => 2;
+        var continuation = task.ContinueWith(ReturnTwo);
+        int result = 2;
+        Assert.That(result, Is.EqualTo(continuation.Result));
+
+        var continuationContinuation = continuation.ContinueWith((x) => x * x);
+        result = 4;
+
+        Assert.That(result, Is.EqualTo(continuationContinuation.Result));
+        pool.ShutDown();
+    }
+
+    [Test]
+    public void ShouldAggregateExceptionWhenDevideByZero()
+    {
+        var pool = new MyThreadPool.MyThreadPool(10);
+        int zero = 0;
+        var task = pool.Submit(() => 1 / zero);
+        int ReturnResult() => task.Result;
+        Assert.Throws<AggregateException>(() => ReturnResult());
+        pool.ShutDown();
+    }
+
+    [Test]
+    public void ShouldExpectedFalseWhenIsCompletedForCancelledTask()
+    {
+        var pool = new MyThreadPool.MyThreadPool(10);
+        var task = pool.Submit(() => 1);
+        pool.ShutDown();
+        bool result = false;
+        Assert.That(result, Is.EqualTo(task.IsCompleted));
+    }
+}
---
+++ ThreadPool/ThreadPool/ThreadPool/MyThreadPool.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Concurrent;
+
+namespace MyThreadPool;
+
+/// <summary>
+/// Class for implementing a thread pool
+/// </summary>
+public class MyThreadPool : IDisposable
+{
+    // Queue for storing tasks in a pool
+    private readonly Queue<Action> queue = new();
+
+    // object for sending a cancellation signal to the CancellationToken token
+    private readonly CancellationTokenSource source = new();
+
+    public CancellationTokenSource Source => source;
+
+    private bool disposed;
+
+    // List for storing pool threads
+    private readonly List<Thread> threads = new();
+
+    public int CountOfThreads => threads.Count;
+
+    // Method that puts the method in the execution queue
+    public void QueueWorkItem(Action func)
+    {
+        lock (queue)
+        {
+            // Добавляем метод в очередь
+            queue.Enqueue(func);
+
+            // Сообщаем потоку, ждущему на Monitor.Wait, что нужно выполнить метод
+            Monitor.Pulse(queue);
+        }
+    }
+
+    // Method that puts the method in the execution queue
+    public void QueueForTaskItem<TResult>(MyTask<TResult> task)
+    {
+        QueueWorkItem(task.Work);
+    }
+
+    /// <summary>
+    /// Method for returning a task to be accepted for execution.
+    /// </summary>
+    /// <typeparam name="TResult">Type of return value</typeparam>
+    /// <param name="func">Method</param>
+    /// <returns>Task presented as an IMyTask<TResult> interface</returns>
+    public IMyTask<TResult> Submit<TResult>(Func<TResult> func)
+    {
+        // Создаем объект класса MyTask
+        var task = new MyTask<TResult>(func, this);
+
+        // Ставим задачу на исполнение в пул потоков
+        QueueWorkItem(task.Work);
+
+        return task;
+    }
+
+    /// <summary>
+    /// Constructor of the MyThreadPool class
+    /// </summary>
+    /// <param name="n">Number of threads in the pool</param>
+    public MyThreadPool(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            threads.Add(new Thread(() =>
+            {
+                Action? result;
+                // Пока операция не была отменена - выполняем работу
+                while (!source.Token.IsCancellationRequested)
+                {
+                    lock (queue)
+                    {
+                        while (queue.Count == 0 && !source.Token.IsCancellationRequested)
+                        {
+                            // Ждем пока очередь станет непустой или операция будет отменена
+                            Monitor.Wait(queue);
+                        }
+
+                        // Берем из очереди метод соответствующий задаче
+                        queue.TryDequeue(out result);
+                    }
+
+                    if (result != null)
+                    {
+                        // Исполняем метод
+                        result!();
+                    }
+                }
+            }));
+        }
+
+        // При создании объекта в нем должно начать работу n потоков
+        foreach(var thread in threads)
+        {
+            thread.Start();
+        }
+    }
+
+    public void ShutDown()
+    {
+        lock(queue)
+        {
+            // Должны вернуть управление когда остановятся все потоки
+            // Хотим оставноить рабочий поток
+            source.Cancel();
+
+            // Сообщаем об этом потоку, который может ждать на Monitor.Wait отмены операции
+            Monitor.PulseAll(queue);
+        }
+
+        
+        // Блокируем поток до завершения потоков
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (this.disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            this.ShutDown();
+        }
+
+        disposed = true;
+    }
+
+    ~MyThreadPool()
+    {
+        Dispose(false);
+    }
+}
+
+public class Solution
+{
+    public static void Main()
+    {
+        var pool = new MyThreadPool(10);
+        int zero = 0;
+        var task = pool.Submit(() => 1 / zero);
+        int ReturnResult() => task.Result;
+        ReturnResult();
+    }
+}
