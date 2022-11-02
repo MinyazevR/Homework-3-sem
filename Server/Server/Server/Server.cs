d8bc16f14162b7f78f2c2be137638bc03588768d
Roman <eeuriset8@gmail.com>
2022-11-02 22:52:37 +0300

    Server, Client classes are written Added tests and comments

---
+++ Server/Server/Server/Server.cs
@@ -0,0 +1,124 @@
+﻿namespace Server;
+
+using System.Net.Sockets;
+using System.Net;
+
+/// <summary>
+/// Class representing the server
+/// </summary>
+public class Server
+{
+    private readonly int port;
+    private readonly IPAddress address;
+
+    /// <summary>
+    /// Сonstructor
+    /// </summary>
+    /// <param name="adress">ip adress</param>
+    /// <param name="port">port</param>
+    public Server(IPAddress adress, int port)
+    {
+        this.address = adress;
+        this.port = port;
+    }
+
+    /// <summary>
+    /// Method for listing files
+    /// </summary>
+    /// <param name="stream">stream</param>
+    /// <param name="path">path to directory</param>
+    /// <returns></returns>
+    private static async Task List(NetworkStream stream, string path)
+    {
+        using var streamWriter = new StreamWriter(stream);
+
+        if (!Directory.Exists(path))
+        {
+            await streamWriter.WriteAsync("-1");
+            await streamWriter.FlushAsync();
+            return;
+        }
+
+        var directories = Directory.GetDirectories(path);
+        var files = Directory.GetFiles(path);
+        var size = directories.Length + files.Length;
+        await streamWriter.WriteAsync(size.ToString());
+        await streamWriter.FlushAsync();
+
+        foreach (var file in files)
+        {
+            await streamWriter.WriteAsync($" {file} false");
+            await streamWriter.FlushAsync();
+        }
+
+        foreach (var directory in directories)
+        {
+            await streamWriter.WriteAsync($" {directory} true");
+            await streamWriter.FlushAsync();
+        }
+    }
+
+    /// <summary>
+    /// Method for get files
+    /// </summary>
+    /// <param name="stream">stream</param>
+    /// <param name="path">path to file</param>
+    /// <returns></returns>
+    private static async Task Get(NetworkStream stream, string path)
+    {
+        using var streamWriter = new StreamWriter(stream);
+        if (!File.Exists(path))
+        {
+            await streamWriter.WriteAsync("-1");
+            await streamWriter.FlushAsync();
+            return;
+        }
+        var size = (new FileInfo(path)).Length;
+        await streamWriter.WriteLineAsync(size.ToString());
+        await streamWriter.FlushAsync();
+        using var fileStream = new FileStream(path, FileMode.Open);
+        await fileStream.CopyToAsync(streamWriter.BaseStream);
+        await streamWriter.FlushAsync();
+    }
+
+    /// <summary>
+    /// Function to start the server (it will start listening for connections from clients)
+    /// </summary>
+    /// <returns></returns>
+    public async Task Start(CancellationTokenSource source)
+    {
+        var tcpListener = new TcpListener(address, port);
+        tcpListener.Start();
+
+        while (!source.Token.IsCancellationRequested)
+        {
+            using var acceptedSocket = tcpListener.AcceptSocket();
+            using var newtworkStream = new NetworkStream(acceptedSocket);
+            using var streamReader = new StreamReader(newtworkStream);
+            var strings = (streamReader.ReadLine())?.Split(' ');
+            if (strings == null)
+            {
+                continue;
+            }
+            switch (strings[0])
+            {
+                case "list":
+                {
+                    await Task.Run(() => List(newtworkStream, strings[1]));
+                    break;
+                }
+                case "get":
+                {
+                    await Task.Run(() => Get(newtworkStream, strings[1]));
+                    break;
+                }
+                default:
+                {
+                    continue;
+                }
+            }
+        }
+
+        tcpListener.Stop();
+    }
+}
---
+++ Server/Server/ServerTest/ServerTest.cs
@@ -0,0 +1,66 @@
+namespace ServerTest;
+
+using System.Net;
+using Server;
+
+public class Tests
+{
+    Server? server;
+    CancellationTokenSource? cancelTokenSource;
+    [SetUp]
+    public void Setup()
+    {
+        server = new Server(IPAddress.Loopback, 80);
+        var cancelTokenSource = new CancellationTokenSource();
+        var serverTask = Task.Run(() => server.Start(cancelTokenSource), cancelTokenSource.Token);
+    }
+
+    [Test]
+    public void ShouldExpectedDirectoryNotFoundExceptionIfListForNonExistentDirectory()
+    {
+        var client = new Client(IPAddress.Loopback, 80);
+        Assert.ThrowsAsync<DirectoryNotFoundException>(() => client.List("./NonExistentDirectory"));
+    }
+
+    [Test]
+    public void ShouldExpectedFileNotFoundExceptionIfGetForNonExistentFile()
+    {
+        var client = new Client(IPAddress.Loopback, 80);
+        Assert.ThrowsAsync<FileNotFoundException>(() => client.Get("NonExistentFile.txt"));
+    }
+
+    [Test]
+    public async Task ShouldExpectedResultsOfGetAreTheSameForDifferentClients()
+    {
+        var client = new Client(IPAddress.Loopback, 80);
+        var newClient = new Client(IPAddress.Loopback, 80);
+        var(size, bytes) = await client.Get("..//..//..//Files//azaza.txt");
+        var(newSize, newBytes) = await newClient.Get("..//..//..//Files//azaza.txt");
+        Assert.Multiple(() =>
+        {
+            Assert.That(size, Is.EqualTo(newSize));
+            Assert.That(bytes, Is.EqualTo(newBytes));
+        });
+    }
+    
+    [Test]
+    public void ShouldExpectedThatMultipleClientsCanAccessTheFile()
+    {
+        var firstClient = new Client(IPAddress.Loopback, 80);
+        var secondClient = new Client(IPAddress.Loopback, 80);
+        var thirdClient = new Client(IPAddress.Loopback, 80);
+        var firstTask = Task.Run(() => firstClient.Get("..//..//..//Files//azaza.txt"));
+        var secondTask = Task.Run(() => secondClient.Get("..//..//..//Files//azaza.txt"));
+        var thirdTask = Task.Run(() => secondClient.Get("..//..//..//Files//azaza.txt"));
+        var (firstSize, firstBytes) = firstTask.Result;
+        var (secondSize, secondBytes) = secondTask.Result;
+        var (thirdSize, thirdBytes) = thirdTask.Result;
+        Assert.Multiple(() =>
+        {
+            Assert.That(firstSize, Is.EqualTo(secondSize));
+            Assert.That(firstSize, Is.EqualTo(thirdSize));
+            Assert.That(firstBytes, Is.EquivalentTo(secondBytes));
+            Assert.That(firstBytes, Is.EquivalentTo(thirdBytes));
+        });
+    }
+}
