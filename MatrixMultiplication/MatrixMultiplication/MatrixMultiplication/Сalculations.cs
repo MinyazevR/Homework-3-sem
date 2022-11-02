14a6cb00cdb5f3898e5299f67a7a84ab46223de8
Roman <eeuriset8@gmail.com>
2022-11-02 14:34:53 +0300

    I forgot to comment on the completed calculations

--- MatrixMultiplication/MatrixMultiplication/MatrixMultiplication/Сalculations.cs
+++ MatrixMultiplication/MatrixMultiplication/MatrixMultiplication/Сalculations.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
 
-/*if (args.Length != 2)
+if (args.Length != 2)
 {
     Console.WriteLine("at the input, the program should receive two files");
     return;
@@ -9,7 +9,7 @@
 var firstMatrix = MatrixMultiplication.Matrix.ReadMatrix(args[0]);
 var secondMatrix = MatrixMultiplication.Matrix.ReadMatrix(args[1]);
 var result = firstMatrix.Multiply(secondMatrix, new MatrixMultiplication.ParallelStrategy());
-result.PrintMatrix("result.txt");*/
+result.PrintMatrix("result.txt");
 
 static (IEnumerable<long>, IEnumerable<long>) Calculate(int size)
 {
@@ -40,10 +40,10 @@ static (IEnumerable<long>, IEnumerable<long>) Calculate(int size)
 }
 
 // тут должны быть размеры матриц, но уже все посчитано
-var sizes = new int[] {8, 16, 32, 64, 128, 256, 512, 1024};
+var sizes = new int[] {/*8, 16, 32, 64, 128, 256, 512, 1024*/};
 
-using StreamWriter stream = new StreamWriter("Calculation.csv");
-stream.Write("Size StandartAverage StandartDeviation ParallelAverage ParallelDeviation");
+//using StreamWriter stream = new StreamWriter("Calculation.csv");
+//stream.Write("Size StandartAverage StandartDeviation ParallelAverage ParallelDeviation");
 
 foreach (var size in sizes)
 {
@@ -52,8 +52,8 @@ foreach (var size in sizes)
     var averageForParallelCalculations = Enumerable.Average(parallelCalculations);
     var varianceForStandardCalculations = Enumerable.Average(standardCalculations.Select(x => x * x)) - averageForStandardCalculations * averageForStandardCalculations;
     var varianceForParallelCalculations = Enumerable.Average(parallelCalculations.Select(x => x * x)) - averageForParallelCalculations * averageForParallelCalculations;
-    stream.WriteLine();
-    stream.Write($"{size} {Math.Round(averageForStandardCalculations, 3)} {Math.Round(Math.Sqrt(varianceForStandardCalculations), 3)} {Math.Round(averageForParallelCalculations, 3)} {Math.Round(Math.Sqrt(varianceForParallelCalculations), 3)}");
+    //stream.WriteLine();
+    //stream.Write($"{size} {Math.Round(averageForStandardCalculations, 3)} {Math.Round(Math.Sqrt(varianceForStandardCalculations), 3)} {Math.Round(averageForParallelCalculations, 3)} {Math.Round(Math.Sqrt(varianceForParallelCalculations), 3)}");
     Console.WriteLine($"Average operation time of standart multiplication for a matrix of size {size} * {size} : {Math.Round(averageForStandardCalculations, 3)}");
     Console.WriteLine($"Standard deviation operation time of standart multiplication for a matrix of size {size} * {size} : {Math.Round(Math.Sqrt(varianceForStandardCalculations), 3)}");
     Console.WriteLine();
@@ -66,65 +66,65 @@ foreach (var size in sizes)
 }
 
 /*
- * Average operation time of standart multiplication for a matrix of size 8 * 8 : 0
+Average operation time of standart multiplication for a matrix of size 8 * 8 : 0
 Standard deviation operation time of standart multiplication for a matrix of size 8 * 8 : 0
 
-Average operation time of parallel multiplication for a matrix of size 8 * 8 : 52,45
-Standard deviation operation time of parallel multiplication for a matrix of size 8 * 8 : 56,882
+Average operation time of parallel multiplication for a matrix of size 8 * 8 : 34,96
+Standard deviation operation time of parallel multiplication for a matrix of size 8 * 8 : 14,788
 
 
 
-Average operation time of standart multiplication for a matrix of size 16 * 16 : 0,03
-Standard deviation operation time of standart multiplication for a matrix of size 16 * 16 : 0,298
+Average operation time of standart multiplication for a matrix of size 16 * 16 : 0
+Standard deviation operation time of standart multiplication for a matrix of size 16 * 16 : 0
 
-Average operation time of parallel multiplication for a matrix of size 16 * 16 : 61,37
-Standard deviation operation time of parallel multiplication for a matrix of size 16 * 16 : 17,762
-
-
-
-Average operation time of standart multiplication for a matrix of size 32 * 32 : 0,37
-Standard deviation operation time of standart multiplication for a matrix of size 32 * 32 : 0,658
-
-Average operation time of parallel multiplication for a matrix of size 32 * 32 : 73,55
-Standard deviation operation time of parallel multiplication for a matrix of size 32 * 32 : 22,835
+Average operation time of parallel multiplication for a matrix of size 16 * 16 : 48,32
+Standard deviation operation time of parallel multiplication for a matrix of size 16 * 16 : 7,571
 
 
 
-Average operation time of standart multiplication for a matrix of size 64 * 64 : 6,47
-Standard deviation operation time of standart multiplication for a matrix of size 64 * 64 : 3,116
+Average operation time of standart multiplication for a matrix of size 32 * 32 : 0,03
+Standard deviation operation time of standart multiplication for a matrix of size 32 * 32 : 0,171
 
-Average operation time of parallel multiplication for a matrix of size 64 * 64 : 74,18
-Standard deviation operation time of parallel multiplication for a matrix of size 64 * 64 : 18,777
-
-
-
-Average operation time of standart multiplication for a matrix of size 128 * 128 : 54,66
-Standard deviation operation time of standart multiplication for a matrix of size 128 * 128 : 16,602
-
-Average operation time of parallel multiplication for a matrix of size 128 * 128 : 106,64
-Standard deviation operation time of parallel multiplication for a matrix of size 128 * 128 : 33,589
+Average operation time of parallel multiplication for a matrix of size 32 * 32 : 45,51
+Standard deviation operation time of parallel multiplication for a matrix of size 32 * 32 : 9,444
 
 
 
-Average operation time of standart multiplication for a matrix of size 256 * 256 : 416,19
-Standard deviation operation time of standart multiplication for a matrix of size 256 * 256 : 50,566
+Average operation time of standart multiplication for a matrix of size 64 * 64 : 5,15
+Standard deviation operation time of standart multiplication for a matrix of size 64 * 64 : 0,517
 
-Average operation time of parallel multiplication for a matrix of size 256 * 256 : 205,77
-Standard deviation operation time of parallel multiplication for a matrix of size 256 * 256 : 40,362
-
-
-
-Average operation time of standart multiplication for a matrix of size 512 * 512 : 4387,75
-Standard deviation operation time of standart multiplication for a matrix of size 512 * 512 : 329,556
-
-Average operation time of parallel multiplication for a matrix of size 512 * 512 : 785,29
-Standard deviation operation time of parallel multiplication for a matrix of size 512 * 512 : 61,57
+Average operation time of parallel multiplication for a matrix of size 64 * 64 : 56,87
+Standard deviation operation time of parallel multiplication for a matrix of size 64 * 64 : 12,45
 
 
 
-Average operation time of standart multiplication for a matrix of size 1024 * 1024 : 39185,98
-Standard deviation operation time of standart multiplication for a matrix of size 1024 * 1024 : 10638,475
+Average operation time of standart multiplication for a matrix of size 128 * 128 : 42,41
+Standard deviation operation time of standart multiplication for a matrix of size 128 * 128 : 0,971
 
-Average operation time of parallel multiplication for a matrix of size 1024 * 1024 : 5329,88
-Standard deviation operation time of parallel multiplication for a matrix of size 1024 * 1024 : 1176,101
+Average operation time of parallel multiplication for a matrix of size 128 * 128 : 72,71
+Standard deviation operation time of parallel multiplication for a matrix of size 128 * 128 : 21,6
+
+
+
+Average operation time of standart multiplication for a matrix of size 256 * 256 : 343,86
+Standard deviation operation time of standart multiplication for a matrix of size 256 * 256 : 4,98
+
+Average operation time of parallel multiplication for a matrix of size 256 * 256 : 132,01
+Standard deviation operation time of parallel multiplication for a matrix of size 256 * 256 : 22,592
+
+
+
+Average operation time of standart multiplication for a matrix of size 512 * 512 : 3423,96
+Standard deviation operation time of standart multiplication for a matrix of size 512 * 512 : 22,406
+
+Average operation time of parallel multiplication for a matrix of size 512 * 512 : 584,37
+Standard deviation operation time of parallel multiplication for a matrix of size 512 * 512 : 33,468
+
+
+
+Average operation time of standart multiplication for a matrix of size 1024 * 1024 : 35753,9
+Standard deviation operation time of standart multiplication for a matrix of size 1024 * 1024 : 26004,022
+
+Average operation time of parallel multiplication for a matrix of size 1024 * 1024 : 4854,64
+Standard deviation operation time of parallel multiplication for a matrix of size 1024 * 1024 : 381,964
 */
