9537dbad4f886dbbc54d008d82b8044d3a42e3fe
Roman <eeuriset8@gmail.com>
2022-11-09 13:11:29 +0300

    fixed tests

--- Test/Test/MD5Test/MD5Test.cs
+++ Test/Test/MD5Test/MD5Test.cs
@@ -8,14 +8,6 @@ public class Tests
     }
 
     [Test]
-    public async Task ShouldReultRemainTheSameForComputeCheckSumForDirectory()
-    {
-        var firstCompute = await Test.MD5.SequentiallyComputeCheckSumForDirectory("..//..//..//Azaza");
-        var secondCompute = await Test.MD5.SequentiallyComputeCheckSumForDirectory("..//..//..//Azaza");
-        Assert.That(firstCompute, Is.EquivalentTo(secondCompute));
-    }
-
-    [Test]
     public async Task ShouldReultRemainTheSameForComputeCheckSumForFile()
     {
         var firstCompute = await Test.MD5.ComputeCheckSumForFile("..//..//..//text.txt");
